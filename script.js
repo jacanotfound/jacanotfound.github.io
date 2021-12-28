b98ee7e6fd3ba488d32b2ac835fb8856a4c77c04
jacanotfound <96659707+jacanotfound@users.noreply.github.com>
2021-12-28 16:42:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('.button');
+let dropmenu = document.querySelector('.dropmenu');
+
+button.addEventListener("click",()=>{
+    dropmenu.classList.toggle('dropmenu--active');
+    button.classList.toggle('button--active')
+});
